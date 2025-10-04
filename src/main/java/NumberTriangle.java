ebf0da01adb3e2028d775ee281ac2783cca38102
samuelcheng <samuelchengca@gmail.com>
2025-10-04 18:48:10 -0400

    changes

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -88,8 +88,19 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
-        // TODO implement this method
-        return -1;
+        if (path.isEmpty()) {
+            return this.root;
+        }
+
+        char direction = path.charAt(0);
+
+        if (direction == 'l') {
+            return left.retrieve(path.substring(1));
+        } else if (direction == 'r') {
+            return right.retrieve(path.substring(1));
+        }  else {
+            return -1;
+
     }
 
     /** Read in the NumberTriangle structure from a file.
